a428d546ea4bf741005a30349429b950ee01b770
joellesenne <jfkles@me.com>
2021-04-21 21:34:34 +0200

    🐛 Fix a bug.

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,18 @@
-const chai = require('chai');
+const chai = require('chai')
 
 /**
  * @load modules UTILS
  */
-const asyncHandler = require('../lib/index');
+const asyncHandler = require('../lib/index')
 
-const { expect } = chai;
+const { expect } = chai
 
 describe('asyncHandler', () => {
   it('should catch exceptions of a function passed into it', async () => {
-    const error = new Error('catch me!', 404);
+    const error = new Error('catch me!', 404)
     const foo = asyncHandler(() => {
-      throw error;
-    });
-    expect(foo).to.throw(error);
-  });
-});
+      throw error
+    })
+    expect(foo).to.throw(error)
+  })
+})
